8bcc9c3c4658095ac119f144585c744d2946de3c
Victor Coda <magma.github@gmail.com>
2018-01-23 16:24:58 +0200

    Implemented Image1DArray object

---
+++ objects/image1DArray.h
@@ -0,0 +1,34 @@
+/*
+Magma - C++1x interface over Khronos Vulkan API.
+Copyright (C) 2018 Victor Coda.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+#pragma once
+#include "image.h"
+
+namespace magma
+{
+    class Image1DArray : public Image
+    {
+    public:
+        Image1DArray(std::shared_ptr<const Device> device,
+            VkFormat format,
+            uint32_t width,
+            uint32_t mipLevels,
+            uint32_t arrayLayers,
+            VkImageUsageFlags usage);
+        virtual VkImageType getType() const override { return VK_IMAGE_TYPE_1D; }
+    };
+} // namespace magma
