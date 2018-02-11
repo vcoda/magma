b637291f60b34d3fe69e1325469230e393f1e9d0
Victor Coda <magma.github@gmail.com>
2018-02-11 21:40:42 +0200

    Implemented StorageBuffer object

---
+++ objects/storageBuffer.cpp
@@ -0,0 +1,52 @@
+/*
+Magma - C++1x interface over Khronos Vulkan API.
+Copyright (C) 2018 Victor Coda.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+#include "storageBuffer.h"
+#include "transferBuffer.h"
+#include "commandBuffer.h"
+#include "queue.h"
+#include "fence.h"
+
+namespace magma
+{
+StorageBuffer::StorageBuffer(std::shared_ptr<const Device> device, VkDeviceSize size,
+    VkBufferCreateFlags flags /* 0 */):
+    Buffer(device, size, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT, flags,
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT)
+{}
+
+StorageBuffer::StorageBuffer(std::shared_ptr<CommandBuffer> copyCmdBuffer, const void *data, VkDeviceSize size,
+    VkBufferCreateFlags flags /* 0 */):
+    Buffer(copyCmdBuffer->getDevice(), size, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, flags,
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT)
+{
+    std::shared_ptr<SourceTransferBuffer> srcBuffer(new SourceTransferBuffer(device, data, size));
+    copyCmdBuffer->begin();
+    {
+        VkBufferCopy region;
+        region.srcOffset = 0;
+        region.dstOffset = 0;
+        region.size = size;
+        vkCmdCopyBuffer(*copyCmdBuffer, *srcBuffer, handle, 1, &region);
+    }
+    copyCmdBuffer->end();
+    std::shared_ptr<Queue> queue(device->getQueue(VK_QUEUE_TRANSFER_BIT, 0));
+    std::shared_ptr<Fence> fence(new Fence(device));
+    queue->submit(copyCmdBuffer, 0, nullptr, nullptr, fence);
+    fence->wait();
+}
+} // namespace magma
---
+++ objects/storageBuffer.h
@@ -0,0 +1,46 @@
+/*
+Magma - C++1x interface over Khronos Vulkan API.
+Copyright (C) 2018 Victor Coda.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+#pragma once
+#include "buffer.h"
+
+namespace magma
+{
+    class CommandBuffer;
+
+    class StorageBuffer : public Buffer
+    {
+    public:
+        StorageBuffer(std::shared_ptr<const Device> device,
+            VkDeviceSize size,
+            VkBufferCreateFlags flags = 0);
+        StorageBuffer(std::shared_ptr<CommandBuffer> copyCmdBuffer,
+            const void *data, VkDeviceSize size,
+            VkBufferCreateFlags flags = 0);
+        template<typename Type>
+        StorageBuffer(std::shared_ptr<CommandBuffer> copyCmdBuffer,
+            const std::vector<Type>& data,
+            VkBufferCreateFlags flags = 0);
+    };
+
+    template<typename Type>
+    inline StorageBuffer::StorageBuffer(std::shared_ptr<CommandBuffer> copyCmdBuffer, 
+        const std::vector<Type>& data, 
+        VkBufferCreateFlags flags /* 0 */):
+        StorageBuffer(copyCmdBuffer, data.data(), static_cast<VkDeviceSize>(sizeof(Type) * data.size()), flags)
+    {}
+} // namespace magma
