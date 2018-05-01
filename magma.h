e2cbccfc9b344f769f62814beff46b4ad0e5be66
Victor Coda <victor.coda@gmail.com>
2018-05-01 13:48:32 +0300

    Include allocator header file

--- magma.h
+++ magma.h
@@ -17,6 +17,8 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 #pragma once
 
+#include "allocator/allocator.h"
+
 #include "objects/buffer.h"
 #include "objects/bufferView.h"
 #include "objects/commandBuffer.h" 
