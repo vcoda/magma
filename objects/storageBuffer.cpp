6f777d848d2f0c7aa46c4d1070bb24dc264919fd
Victor Coda <victor.coda@gmail.com>
2018-06-09 19:06:58 +0300

    Optimized include

--- objects/storageBuffer.cpp
+++ objects/storageBuffer.cpp
@@ -17,6 +17,7 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 #include "storageBuffer.h"
 #include "srcTransferBuffer.h"
+#include "device.h"
 #include "commandBuffer.h"
 #include "queue.h"
 #include "fence.h"
--- objects/storageBuffer.h
+++ objects/storageBuffer.h
@@ -16,8 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 #pragma once
+#include <vector>
 #include "buffer.h"
-#include "device.h"
 
 namespace magma
 {
