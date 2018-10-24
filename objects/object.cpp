a395acbcc00ffb96a2c5fb829484a2c945a6e3ad
Victor Coda <victor.coda@gmail.com>
2018-10-24 23:22:51 +0300

    Added missing include

--- objects/object.cpp
+++ objects/object.cpp
@@ -15,6 +15,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
+#include <cstring>
 #include "object.h"
 #include "device.h"
 #include "../allocator/objectAllocator.h"
