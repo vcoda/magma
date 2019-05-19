d8a966c72e2ad737c2afdf9b8af46397cf37a28f
Victor Coda <victor.coda@gmail.com>
2019-05-20 02:59:28 +0300

    Use namespace std with size_t type

--- internal/hash.h
+++ internal/hash.h
@@ -24,20 +24,20 @@ namespace magma
     namespace internal
     {
         // https://www.boost.org/doc/libs/1_46_1/doc/html/hash/reference.html#boost.hash_combine
-        constexpr void hashCombine(size_t& seed, size_t hash) noexcept
+        constexpr void hashCombine(std::size_t& seed, std::size_t hash) noexcept
         {
             seed ^= (hash + 0x9e3779b9 + (seed << 6) + (seed >> 2));
         }
 
         template<typename Type>
-        constexpr void hashNext(size_t& seed, const Type& arg) noexcept
+        constexpr void hashNext(std::size_t& seed, const Type& arg) noexcept
         {
             std::hash<Type> hasher;
             hashCombine(seed, hasher(arg));
         }
 
         template<typename Type, typename... Args>
-        constexpr void hashNext(size_t& seed, const Type& arg, Args... args) noexcept
+        constexpr void hashNext(std::size_t& seed, const Type& arg, Args... args) noexcept
         {
             std::hash<Type> hasher;
             hashCombine(seed, hasher(arg));
@@ -45,21 +45,21 @@ namespace magma
         }
 
         template<typename Type, typename... Args>
-        constexpr size_t hashArgs(const Type& arg, Args... args) noexcept
+        constexpr std::size_t hashArgs(const Type& arg, Args... args) noexcept
         {
             std::hash<Type> hasher;
-            size_t value = 0;
+            std::size_t value = 0;
             hashCombine(value, hasher(arg));
             hashNext(value, args...);
             return value;
         }
 
         template<typename Type>
-        constexpr size_t hashArray(const Type *const array, size_t count) noexcept
+        constexpr std::size_t hashArray(const Type *const array, std::size_t count) noexcept
         {
             std::hash<Type> hasher;
-            size_t value = 0;
-            for (size_t i = 0; i < count; ++i)
+            std::size_t value = 0;
+            for (std::size_t i = 0; i < count; ++i)
                 hashCombine(value, hasher(array[i]));
             return value;
         }
