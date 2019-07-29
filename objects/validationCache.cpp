6b3595f08cced0cc7cbc4e423912ea06d90f422c
Victor Coda <victor.coda@gmail.com>
2019-07-30 00:35:19 +0300

    Fixed object type of ValidationCache

--- objects/validationCache.cpp
+++ objects/validationCache.cpp
@@ -29,7 +29,7 @@ namespace magma
 ValidationCache::ValidationCache(std::shared_ptr<Device> device,
     size_t dataSize, const void *cacheData,
     std::shared_ptr<IAllocator> allocator /* nullptr */):
-    NonDispatchable(VK_OBJECT_TYPE_PIPELINE_CACHE, std::move(device), std::move(allocator))
+    NonDispatchable(VK_OBJECT_TYPE_VALIDATION_CACHE_EXT, std::move(device), std::move(allocator))
 {
     MAGMA_DEVICE_EXTENSION(vkCreateValidationCacheEXT, VK_EXT_VALIDATION_CACHE_EXTENSION_NAME);
     VkValidationCacheCreateInfoEXT info;
