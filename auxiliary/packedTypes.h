d2ddb746cc782e90949a7b1a0c263f65af04f5dd
Victor Coda <victor.coda@gmail.com>
2019-10-24 02:47:48 +0300

    Unsigned normal do not contracted explicitly by *0.5+0.5. Require SSE 4.1 if SSE is used. packedTypes.h file moved to auxiliary directory. Code refactoring.

--- auxiliary/packedTypes.h
+++ auxiliary/packedTypes.h
@@ -17,24 +17,21 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 #pragma once
 #include <cstdint>
-#ifdef MAGMA_SSE2_INTRINSICS
+#ifdef MAGMA_SSE
 #include <xmmintrin.h>
-#include <emmintrin.h>
-#ifdef MAGMA_SSE4_INTRINSICS
 #include <smmintrin.h>
-#endif
 #else
 #include <algorithm>
-#endif // MAGMA_SSE2_INTRINSICS
+#endif // !MAGMA_SSE
 
 namespace magma
 {
     namespace aux
     {
-        /* Unsigned 32-bit normal that reflects
+        /* Unsigned 32-bit normal in the range [0,1] that reflects
            VK_FORMAT_A2B10G10R10_UNORM_PACK32 format. */
 
-        struct UnsignedNormalX10y10z10w2
+        struct UnsignedNormalW2z10y10x10
         {
             union
             {
@@ -48,21 +45,35 @@ namespace magma
                 uint32_t v;
             };
 
-            UnsignedNormalX10y10z10w2() noexcept = default;
-            UnsignedNormalX10y10z10w2(float x, float y, float z, float w = 0.f) noexcept
+            UnsignedNormalW2z10y10x10() noexcept = default;
+            UnsignedNormalW2z10y10x10(float x, float y, float z, float w = 0.f) noexcept
+#if defined(MAGMA_SSE)
+                : UnsignedNormalW2z10y10x10(_mm_set_ps(w, z, y, x)) {}
+#else
             {
-                constexpr float l[4] = {-1.f, -1.f, -1.f, 0.f};
+                x = std::min(std::max(0.f, x), 1.f);
+                y = std::min(std::max(0.f, y), 1.f);
+                z = std::min(std::max(0.f, z), 1.f);
+                w = std::min(std::max(0.f, w), 1.f);
+                x = std::roundf(x * 1023.f);
+                y = std::roundf(y * 1023.f);
+                z = std::roundf(z * 1023.f);
+                w = std::roundf(w * 3.f);
+                this->x = static_cast<uint32_t>(x);
+                this->y = static_cast<uint32_t>(y);
+                this->z = static_cast<uint32_t>(z);
+                this->w = static_cast<uint32_t>(w);
+            }
+#endif // !MAGMA_SSE
+#if defined(MAGMA_SSE)
+            UnsignedNormalW2z10y10x10(__m128 v) noexcept
+            {
+                constexpr float l[4] = {0.f, 0.f, 0.f, 0.f};
                 constexpr float h[4] = {1.f, 1.f, 1.f, 1.f};
-                constexpr float mul[4] = {.5f, .5f, .5f, 1.f};
-                constexpr float add[4] = {.5f, .5f, .5f, 0.f};
                 constexpr float scale[4] = {1023.f, 1023.f, 1023.f, 3.f};
-#           ifdef MAGMA_SSE2_INTRINSICS
                 __m128 lo = _mm_load_ps(l);
                 __m128 hi = _mm_load_ps(h);
-                __m128 v = _mm_set_ps(w, z, y, x);
-                v = _mm_min_ps(_mm_max_ps(v, lo), hi);
-                v = _mm_mul_ps(v, _mm_load_ps(mul));
-                v = _mm_add_ps(v, _mm_load_ps(add));
+                v = _mm_min_ps(_mm_max_ps(lo, v), hi);
                 v = _mm_mul_ps(v, _mm_load_ps(scale));
                 v = _mm_round_ps(v, _MM_ROUND_NEAREST);
                 __m128i vi = _mm_cvtps_epi32(v);
@@ -71,30 +82,16 @@ namespace magma
                 this->y = p[1];
                 this->z = p[2];
                 this->w = p[3];
-#           else
-                x = std::min(std::max(x, l[0]), h[0]);
-                y = std::min(std::max(y, l[1]), h[1]);
-                z = std::min(std::max(z, l[2]), h[2]);
-                w = std::min(std::max(w, l[3]), h[3]);
-                x = x * .5f + .5f;
-                y = y * .5f + .5f;
-                z = z * .5f + .5f;
-                x = std::roundf(x * scale[0]);
-                y = std::roundf(y * scale[1]);
-                z = std::roundf(z * scale[2]);
-                w = std::roundf(w * scale[3]);
-                this->x = static_cast<uint32_t>(x);
-                this->y = static_cast<uint32_t>(y);
-                this->z = static_cast<uint32_t>(z);
-                this->w = static_cast<uint32_t>(w);
-#           endif // MAGMA_SSE2_INTRINSICS
             }
+#elif defined(MAGMA_NEON)
+            // TODO
+#endif // MAGMA_NEON
         };
 
-        /* Signed 32-bit normal that reflects
+        /* Signed 32-bit normal in the range[-1,1] that reflects
            VK_FORMAT_A2B10G10R10_SNORM_PACK32 format. */
 
-        struct SignedNormalX10y10z10w2
+        struct SignedNormalW2z10y10x10
         {
             union
             {
@@ -108,17 +105,35 @@ namespace magma
                 int32_t v;
             };
 
-            SignedNormalX10y10z10w2() noexcept = default;
-            SignedNormalX10y10z10w2(float x, float y, float z, float w = 0.f) noexcept
+            SignedNormalW2z10y10x10() noexcept = default;
+            SignedNormalW2z10y10x10(float x, float y, float z, float w = 0.f) noexcept
+#if defined(MAGMA_SSE)
+                : SignedNormalW2z10y10x10(_mm_set_ps(w, z, y, x)) {}
+#else
+            {
+                x = std::min(std::max(-1.f, x), 1.f);
+                y = std::min(std::max(-1.f, y), 1.f);
+                z = std::min(std::max(-1.f, z), 1.f);
+                w = std::min(std::max(0.f, w), 1.f);
+                x = std::roundf(x * 511.f);
+                y = std::roundf(y * 511.f);
+                z = std::roundf(z * 511.f);
+                w = std::roundf(w * 3.f);
+                this->x = static_cast<int32_t>(x);
+                this->y = static_cast<int32_t>(y);
+                this->z = static_cast<int32_t>(z);
+                this->w = static_cast<int32_t>(w);
+            }
+#endif // !MAGMA_SSE
+#if defined(MAGMA_SSE)
+            SignedNormalW2z10y10x10(__m128 v) noexcept
             {
                 constexpr float l[4] = {-1.f, -1.f, -1.f, 0.f};
                 constexpr float h[4] = {1.f, 1.f, 1.f, 1.f};
                 constexpr float scale[4] = {511.f, 511.f, 511.f, 3.f};
-#           ifdef MAGMA_SSE2_INTRINSICS
                 __m128 lo = _mm_load_ps(l);
                 __m128 hi = _mm_load_ps(h);
-                __m128 v = _mm_set_ps(w, z, y, x);
-                v = _mm_min_ps(_mm_max_ps(v, lo), hi);
+                v = _mm_min_ps(_mm_max_ps(lo, v), hi);
                 v = _mm_mul_ps(v, _mm_load_ps(scale));
                 v = _mm_round_ps(v, _MM_ROUND_NEAREST);
                 __m128i vi = _mm_cvtps_epi32(v);
@@ -127,21 +142,10 @@ namespace magma
                 this->y = p[1];
                 this->z = p[2];
                 this->w = p[3];
-#           else
-                x = std::min(std::max(x, l[0]), h[0]);
-                y = std::min(std::max(y, l[1]), h[1]);
-                z = std::min(std::max(z, l[2]), h[2]);
-                w = std::min(std::max(w, l[3]), h[3]);
-                x = std::roundf(x * scale[0]);
-                y = std::roundf(y * scale[1]);
-                z = std::roundf(z * scale[2]);
-                w = std::roundf(w * scale[3]);
-                this->x = static_cast<int32_t>(x);
-                this->y = static_cast<int32_t>(y);
-                this->z = static_cast<int32_t>(z);
-                this->w = static_cast<int32_t>(w);
-#           endif // MAGMA_SSE2_INTRINSICS
             }
+#elif defined(MAGMA_NEON)
+            // TODO
+#endif // MAGMA_NEON
         };
     } // namespace aux
 } // namespace magma
