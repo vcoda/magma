c37759a84a3c6a46318a2c1f3f26b07bbfb8ac78
Victor Coda <victor.coda@gmail.com>
2020-02-04 01:01:25 +0200

    Use constructor delegation

--- objects/pipelineLayout.cpp
+++ objects/pipelineLayout.cpp
@@ -28,25 +28,8 @@ namespace magma
 {
 PipelineLayout::PipelineLayout(std::shared_ptr<Device> device, const PushConstantRange& pushConstantRange,
     std::shared_ptr<IAllocator> allocator /* nullptr */):
-    NonDispatchable(VK_OBJECT_TYPE_PIPELINE_LAYOUT, std::move(device), std::move(allocator))
-{
-    VkPipelineLayoutCreateInfo info;
-    info.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-    info.pNext = nullptr;
-    info.flags = 0;
-    info.setLayoutCount = 0;
-    info.pSetLayouts = nullptr;
-    info.pushConstantRangeCount = 1;
-    info.pPushConstantRanges = &pushConstantRange;
-    const VkResult create = vkCreatePipelineLayout(MAGMA_HANDLE(device), &info, MAGMA_OPTIONAL_INSTANCE(allocator), &handle);
-    MAGMA_THROW_FAILURE(create, "failed to create pipeline layout");
-    hash = detail::hashArgs(
-        info.sType,
-        info.flags,
-        info.setLayoutCount,
-        info.pushConstantRangeCount);
-    detail::hashCombine(hash, pushConstantRange.hash());
-}
+    PipelineLayout(std::move(device), {pushConstantRange}, std::move(allocator))
+{}
 
 PipelineLayout::PipelineLayout(std::shared_ptr<Device> device,
     const std::initializer_list<PushConstantRange>& pushConstantRanges /* {} */,
