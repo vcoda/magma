c07d6493acca35ec3aac399b71a890f423d3d4d8
Victor Coda <victor.coda@gmail.com>
2020-05-20 16:59:11 +0300

    Disable globally annoying warning "integral constant overflow"

--- core/hashing/fnv1.h
+++ core/hashing/fnv1.h
@@ -16,6 +16,9 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 #pragma once
+#ifdef _MSC_VER
+#pragma warning(disable: 4307) // integral constant overflow
+#endif
 
 namespace magma
 {
