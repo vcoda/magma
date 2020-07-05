7323eec580d3515357fd1fede40cddcc27d232b2
Victor Coda <victor.coda@gmail.com>
2020-07-05 19:10:20 +0300

    Implemented MultiAttachmentFramebuffer class for MRT scenario

---
+++ auxiliary/multiAttachmentFramebuffer.cpp
@@ -0,0 +1,91 @@
+/*
+Magma - abstraction layer to facilitate usage of Khronos Vulkan API.
+Copyright (C) 2018-2020 Victor Coda.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+#include "pch.h"
+#pragma hdrstop
+#include "multiAttachmentFramebuffer.h"
+#include "../objects/device.h"
+#include "../objects/image2DAttachment.h"
+#include "../objects/imageView.h"
+#include "../objects/renderPass.h"
+#include "../objects/framebuffer.h"
+#include "../misc/format.h"
+
+namespace magma
+{
+namespace aux
+{
+MultiAttachmentFramebuffer::MultiAttachmentFramebuffer(std::shared_ptr<Device> device,
+    const std::vector<VkFormat>& colorAttachmentFormats, const VkFormat depthStencilFormat,
+    const VkExtent2D& extent, bool shouldReadDepth,
+    std::shared_ptr<IAllocator> allocator /* nullptr */,
+    const std::vector<VkComponentMapping>& swizzles /* {} */):
+    Framebuffer(1)
+{
+    constexpr VkComponentMapping dontSwizzle = {
+        VK_COMPONENT_SWIZZLE_IDENTITY,
+        VK_COMPONENT_SWIZZLE_IDENTITY,
+        VK_COMPONENT_SWIZZLE_IDENTITY,
+        VK_COMPONENT_SWIZZLE_IDENTITY};
+    uint32_t index = 0;
+    for (const VkFormat colorFormat : colorAttachmentFormats)
+    {   // Create color attachment
+        attachments.emplace_back(std::make_shared<ColorAttachment>(device, colorFormat, extent,
+            1, 1, true, allocator));
+        // Create color view
+        attachmentViews.emplace_back(std::make_shared<ImageView>(attachments.back(),
+            swizzles.empty() ? dontSwizzle : swizzles[index++],
+            allocator));
+    }
+    if (depthStencilFormat != VK_FORMAT_UNDEFINED)
+    {   // Create depth/stencil attachment
+        attachments.emplace_back(std::make_shared<DepthStencilAttachment>(device, depthStencilFormat, extent,
+            1, 1, shouldReadDepth, allocator));
+        // Create depth/stencil view
+        attachmentViews.push_back(std::make_shared<ImageView>(attachments.back(), VkComponentMapping{
+            VK_COMPONENT_SWIZZLE_IDENTITY,
+            VK_COMPONENT_SWIZZLE_IDENTITY,
+            VK_COMPONENT_SWIZZLE_IDENTITY,
+            VK_COMPONENT_SWIZZLE_IDENTITY},
+            allocator));
+    }
+    // Setup attachment descriptions
+    std::vector<AttachmentDescription> attachmentDescriptions;
+    for (const VkFormat format : colorAttachmentFormats)
+    {
+        attachmentDescriptions.emplace_back(format, 1,
+            op::clearStore, // Clear color, store
+            op::dontCare, // Stencil not applicable
+            VK_IMAGE_LAYOUT_UNDEFINED, // Don't care
+            VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL); // Color image will be transitioned to when a render pass instance ends
+    }
+    if (depthStencilFormat != VK_FORMAT_UNDEFINED)
+    {   // Choose optimal depth/stencil layout
+        const VkImageLayout finalLayout = finalDepthStencilLayout(device, depthStencilFormat, shouldReadDepth);
+        const Format format(depthStencilFormat);
+        attachmentDescriptions.emplace_back(depthStencilFormat, 1,
+            op::clearStore, // Clear depth, store
+            format.depthStencil() || format.stencil() ? op::clearStore : op::dontCare,
+            VK_IMAGE_LAYOUT_UNDEFINED, // Don't care
+            finalLayout); // Depth image will be transitioned to when a render pass instance ends
+    }
+    // Create color/depth framebuffer
+    renderPass = std::make_shared<RenderPass>(std::move(device), attachmentDescriptions);
+    framebuffer = std::make_shared<magma::Framebuffer>(renderPass, attachmentViews, 0, std::move(allocator));
+}
+} // namespace aux
+} // namespace magma
---
+++ auxiliary/multiAttachmentFramebuffer.h
@@ -0,0 +1,54 @@
+/*
+Magma - abstraction layer to facilitate usage of Khronos Vulkan API.
+Copyright (C) 2018-2020 Victor Coda.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+#pragma once
+#include "baseFramebuffer.h"
+
+namespace magma
+{
+    class Device;
+    class Image2D;
+    class ImageView;
+    class IAllocator;
+
+    namespace aux
+    {
+        /* Auxiliary frame buffer that is responsible for management of
+           multiple color attachments and depth/stencil buffer. This allows
+           the rendering pipeline to draw to multiple render targets at once. */
+
+        class MultiAttachmentFramebuffer : public Framebuffer
+        {
+        public:
+            explicit MultiAttachmentFramebuffer(std::shared_ptr<Device> device,
+                const std::vector<VkFormat>& colorAttachmentFormats,
+                const VkFormat depthStencilFormat,
+                const VkExtent2D& extent,
+                bool shouldReadDepth,
+                std::shared_ptr<IAllocator> allocator = nullptr,
+                const std::vector<VkComponentMapping>& swizzles = {});
+            std::shared_ptr<ImageView> getAttachmentView(uint32_t index) noexcept { return attachmentViews[index]; }
+            std::shared_ptr<const ImageView> getAttachmentView(uint32_t index) const noexcept { return attachmentViews[index]; }
+            std::shared_ptr<ImageView> getDepthStencilView() noexcept { return attachmentViews.back(); }
+            std::shared_ptr<const ImageView> getDepthStencilView() const noexcept { return attachmentViews.back(); }
+
+        private:
+            std::vector<std::shared_ptr<Image2D>> attachments;
+            std::vector<std::shared_ptr<ImageView>> attachmentViews;
+        };
+    } // namespace aux
+} // namespace magma
