f7880ccc4398471d0bac10f3cec0ec3083d434f0
v.koda <v.koda@dragonlk.com>
2021-04-06 23:20:05 +0300

    Implemented AccelerationStructureInstance object. Implemented AccelerationStructureInstanceBuffer object to store/update instance transforms and other properties in acceleration structure.

---
+++ objects/accelerationStructureInstanceBuffer.cpp
@@ -0,0 +1,96 @@
+/*
+Magma - abstraction layer to facilitate usage of Khronos Vulkan API.
+Copyright (C) 2018-2020 Victor Coda.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+#include "pch.h"
+#pragma hdrstop
+#include "accelerationStructureInstanceBuffer.h"
+#include "accelerationStructure.h"
+#include "deviceMemory.h"
+#include "srcTransferBuffer.h"
+#include "commandBuffer.h"
+
+namespace magma
+{
+#ifdef VK_NV_ray_tracing
+AccelerationStructureInstance::AccelerationStructureInstance() noexcept
+{
+    constexpr float identity[3][4] = {
+        {1.f, 0.f, 0.f, 0.f},
+        {0.f, 1.f, 0.f, 0.f},
+        {0.f, 0.f, 1.f, 0.f}
+    };
+    memcpy(transform.matrix, identity, sizeof(identity));
+    instanceCustomIndex = 0;
+    mask = 0xFF;
+    instanceShaderBindingTableRecordOffset = 0;
+    flags = 0;
+    accelerationStructureReference = VK_NULL_HANDLE;
+}
+
+void AccelerationStructureInstance::setAccelerationStructure(std::shared_ptr<AccelerationStructure> accelerationStructure)
+{
+    accelerationStructureReference = accelerationStructure->getReferenceHandle();
+}
+
+AccelerationStructureInstanceBuffer::AccelerationStructureInstanceBuffer(std::shared_ptr<Device> device, uint32_t instanceCount,
+    VkBufferCreateFlags flags /* 0 */,
+    const Sharing& sharing /* default */,
+    std::shared_ptr<IAllocator> allocator /* nullptr */,
+    CopyMemoryFunction copyFn /* nullptr */):
+    RayTracingBuffer(device, 
+        sizeof(AccelerationStructureInstance) * instanceCount,
+        flags, sharing, allocator),
+    stagingBuffer(std::make_shared<SrcTransferBuffer>(std::move(device),
+        sizeof(AccelerationStructureInstance) * instanceCount, nullptr,
+        flags, sharing, std::move(allocator))),
+    instances(instanceCount)
+{
+    static_assert(sizeof(AccelerationStructureInstance) == sizeof(VkAccelerationStructureInstanceKHR), "invalid structure size");
+    for (uint32_t instanceIndex = 0; instanceIndex < instanceCount; ++instanceIndex)
+        instances[instanceIndex].setCustomIndex(instanceIndex);
+}
+
+bool AccelerationStructureInstanceBuffer::update(std::shared_ptr<CommandBuffer> cmdBuffer,
+    uint32_t firstInstance, uint32_t instanceCount)
+{
+    if (firstInstance + instanceCount > getInstanceCount())
+        return false;
+    void *dst = stagingBuffer->getMemory()->map(firstInstance * sizeof(AccelerationStructureInstance));
+    if (dst)
+    {
+        const AccelerationStructureInstance *src = instances.data() + firstInstance;
+        memcpy(dst, src, sizeof(AccelerationStructureInstance) * instanceCount);
+        stagingBuffer->getMemory()->unmap();
+    }
+    // Copy staging buffer to instance buffer
+    VkBufferCopy region;
+    region.srcOffset = firstInstance * sizeof(AccelerationStructureInstance);
+    region.dstOffset = region.srcOffset;
+    region.size = sizeof(AccelerationStructureInstance) * instanceCount;
+    cmdBuffer->copyBuffer(stagingBuffer, shared_from_this(), region);
+    // Make sure the copy to instance buffer is finished before triggering the acceleration structure build
+    constexpr magma::MemoryBarrier instanceBufferBarrier(
+        VK_ACCESS_TRANSFER_WRITE_BIT,
+        VK_ACCESS_ACCELERATION_STRUCTURE_READ_BIT_NV);
+    cmdBuffer->pipelineBarrier(
+        VK_PIPELINE_STAGE_TRANSFER_BIT,
+        VK_PIPELINE_STAGE_ACCELERATION_STRUCTURE_BUILD_BIT_NV,
+        instanceBufferBarrier);
+    return true;
+}
+#endif // VK_NV_ray_tracing
+} // namespace magma
---
+++ objects/accelerationStructureInstanceBuffer.h
@@ -0,0 +1,77 @@
+/*
+Magma - abstraction layer to facilitate usage of Khronos Vulkan API.
+Copyright (C) 2018-2020 Victor Coda.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+#pragma once
+#include "rayTracingBuffer.h"
+
+namespace magma
+{
+    class AccelerationStructure;
+    class CommandBuffer;
+    class SrcTransferBuffer;
+
+#ifdef VK_NV_ray_tracing
+    typedef VkTransformMatrixKHR TransformMatrix;
+
+    /* Single acceleration structure instance for building into an acceleration structure geometry. */
+
+    class AccelerationStructureInstance : public VkAccelerationStructureInstanceKHR
+    {
+    public:
+        AccelerationStructureInstance() noexcept;
+        void setTransform(const TransformMatrix& transform) noexcept { this->transform = transform; }
+        void setCustomIndex(uint32_t instanceIndex) noexcept { instanceCustomIndex = instanceIndex; }
+        void setVisibilityMask(uint8_t mask) noexcept { this->mask = mask; }
+        void enableTriangleCull() noexcept { flags &= ~VK_GEOMETRY_INSTANCE_TRIANGLE_CULL_DISABLE_BIT_NV; }
+        void disableTriangleCull() noexcept { flags |= VK_GEOMETRY_INSTANCE_TRIANGLE_CULL_DISABLE_BIT_NV; }
+        bool triangleCullEnabled() const noexcept { return flags & VK_GEOMETRY_INSTANCE_TRIANGLE_CULL_DISABLE_BIT_NV; }
+        void setTriangleFrontCW() noexcept { flags &= ~VK_GEOMETRY_INSTANCE_TRIANGLE_FRONT_COUNTERCLOCKWISE_BIT_NV; }
+        void setTriangleFrontCCW() noexcept { flags |= VK_GEOMETRY_INSTANCE_TRIANGLE_FRONT_COUNTERCLOCKWISE_BIT_NV; }
+        bool triangleFrontCCW() const noexcept { return flags & VK_GEOMETRY_INSTANCE_TRIANGLE_FRONT_COUNTERCLOCKWISE_BIT_NV; }
+        void forceGeometryOpaque(bool opaque) noexcept { if (opaque) flags |= VK_GEOMETRY_INSTANCE_FORCE_OPAQUE_BIT_NV; 
+                                                         else flags &= ~VK_GEOMETRY_INSTANCE_FORCE_OPAQUE_BIT_NV; }
+        bool geometryForcedOpaque() const noexcept { return flags & VK_GEOMETRY_INSTANCE_FORCE_OPAQUE_BIT_NV; }
+        void forceGeometryNoOpaque(bool noOpaque) noexcept { if (noOpaque) flags |= VK_GEOMETRY_INSTANCE_FORCE_NO_OPAQUE_BIT_NV;
+                                                           else flags &= ~VK_GEOMETRY_INSTANCE_FORCE_NO_OPAQUE_BIT_NV; }
+        bool geometryForcedNoOpaque() const noexcept { return flags & VK_GEOMETRY_INSTANCE_FORCE_NO_OPAQUE_BIT_NV; }
+        void setAccelerationStructure(std::shared_ptr<AccelerationStructure> accelerationStructure);
+    };
+
+    /* Buffer containing an array of VkAccelerationStructureInstanceKHR structures 
+       defining acceleration structures. */
+
+    class AccelerationStructureInstanceBuffer : public RayTracingBuffer
+    {
+    public:
+        explicit AccelerationStructureInstanceBuffer(std::shared_ptr<Device> device,
+            uint32_t instanceCount,
+            VkBufferCreateFlags flags = 0,
+            const Sharing& sharing = Sharing(),
+            std::shared_ptr<IAllocator> allocator = nullptr,
+            CopyMemoryFunction copyFn = nullptr);
+        uint32_t getInstanceCount() const noexcept { return (uint32_t)instances.size(); }
+        AccelerationStructureInstance& getInstance(uint32_t instanceIndex) noexcept { return instances[instanceIndex]; }
+        bool update(std::shared_ptr<CommandBuffer> cmdBuffer,
+            uint32_t firstInstance, 
+            uint32_t instanceCount);
+
+    private:
+        std::shared_ptr<SrcTransferBuffer> stagingBuffer;
+        std::vector<AccelerationStructureInstance> instances;
+    };
+#endif // VK_NV_ray_tracing
+} // namespace magma
