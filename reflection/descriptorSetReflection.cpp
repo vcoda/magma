5cbde5a1e93be957217e7c8dc8c441549bc42c70
v.koda <v.koda@dragonlk.com>
2021-05-20 17:12:20 +0300

    Implemented magma::reflection::DescriptorSetLayout and DescripotSet objects

--- reflection/descriptorSetReflection.cpp
+++ reflection/descriptorSetReflection.cpp
@@ -18,10 +18,76 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 #include "pch.h"
 #pragma hdrstop
 #include "descriptorSetReflection.h"
+#include "descriptorSetBinding.h"
+#include "../objects/descriptorPool.h"
+#include "../objects/descriptorSetLayout.h"
+#include "../exceptions/errorResult.h"
 
 namespace magma
 {
 namespace reflection
 {
+void DescriptorSetLayout::constructSetLayout(std::shared_ptr<magma::Device> device, uint32_t stageFlags,
+    std::shared_ptr<IAllocator> allocator /* nullptr */)
+{
+    std::vector<VkDescriptorSetLayoutBinding> bindings;
+    for (auto binding : getBindings())
+    {
+        binding->stageFlags = stageFlags; 
+        bindings.push_back(*binding);
+    }
+    layout = std::make_shared<magma::DescriptorSetLayout>(
+        std::move(device), bindings, 0, std::move(allocator));
+}
+
+DescriptorSet::DescriptorSet(std::shared_ptr<magma::DescriptorPool> descriptorPool, DescriptorSetLayout& setLayout, uint32_t stageFlags,
+    std::shared_ptr<IAllocator> allocator /* nullptr */):
+    NonDispatchable(VK_OBJECT_TYPE_DESCRIPTOR_SET, descriptorPool->getDevice(), std::move(allocator)),
+    descriptorPool(std::move(descriptorPool)),
+    bindings(setLayout.getBindings())
+{
+    setLayout.constructSetLayout(this->descriptorPool->getDevice(), stageFlags, allocator);
+    const VkDescriptorSetLayout dereferencedSetLayouts[1] = {*setLayout.getLayout()};
+    VkDescriptorSetAllocateInfo allocInfo;
+    allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+    allocInfo.pNext = nullptr;
+    allocInfo.descriptorPool = *this->descriptorPool;
+    allocInfo.descriptorSetCount = 1;
+    allocInfo.pSetLayouts = dereferencedSetLayouts;
+    const VkResult result = vkAllocateDescriptorSets(MAGMA_HANDLE(device), &allocInfo, &handle);
+    MAGMA_THROW_FAILURE(result, "failed to allocate descriptor set");
+    for (auto binding : this->bindings)
+        binding->descriptorWrite.dstSet = handle; // Assign handle to write descriptor
+}
+
+DescriptorSet::~DescriptorSet()
+{
+    vkFreeDescriptorSets(MAGMA_HANDLE(device), *descriptorPool, 1, &handle);
+}
+
+bool DescriptorSet::dirty() const noexcept
+{
+    for (auto binding : bindings)
+    {
+        if (binding->dirty())
+            return true;
+    }
+    return false;
+}
+
+void DescriptorSet::update()
+{
+    if (dirty())
+    {
+        std::vector<VkWriteDescriptorSet> descriptorWrites;
+        descriptorWrites.reserve(bindings.size());
+        for (auto binding : bindings)
+        {
+            descriptorWrites.push_back(binding->descriptorWrite);
+            binding->updated = false;
+        }
+        device->updateDescriptorSets(descriptorWrites);
+    }
+}
 } // namespace reflection
 } // namespace magma
--- reflection/descriptorSetReflection.h
+++ reflection/descriptorSetReflection.h
@@ -16,12 +16,56 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 #pragma once
-#include "../core/noncopyable.h"
 #include "../objects/nondispatchable.h"
 
 namespace magma
 {
+    class Device;
+    class DescriptorPool;
+    class DescriptorSetLayout;
+    class IAllocator;
+
     namespace reflection
     {
+        class DescriptorSetLayoutBinding;
+
+        class DescriptorSetLayout
+        {
+        public:
+            template<class... DescriptorSetLayoutBinding>
+            DescriptorSetLayout(DescriptorSetLayoutBinding&&... args)
+            {
+                std::initializer_list<int>{
+                    (bindings.push_back(std::forward<DescriptorSetLayoutBinding>(args)), void(), 0)...
+                };
+            }
+            void constructSetLayout(std::shared_ptr<magma::Device> device,
+                uint32_t stageFlags,
+                std::shared_ptr<IAllocator> allocator /* nullptr */);
+            const std::vector<DescriptorSetLayoutBinding *> getBindings() const noexcept { return bindings; }
+            std::shared_ptr<magma::DescriptorSetLayout> getLayout() const noexcept { return layout; }
+
+        private:
+            std::vector<DescriptorSetLayoutBinding *> bindings;
+            std::shared_ptr<magma::DescriptorSetLayout> layout;
+        };
+
+        class DescriptorSet : public NonDispatchable<VkDescriptorSet>
+        {
+        public:
+            explicit DescriptorSet(std::shared_ptr<magma::DescriptorPool> descriptorPool,
+                DescriptorSetLayout& setLayout,
+                uint32_t stageFlags,
+                std::shared_ptr<IAllocator> allocator = nullptr);
+            ~DescriptorSet();
+            bool dirty() const noexcept;
+            void update();
+
+        private:
+            std::shared_ptr<magma::DescriptorPool> descriptorPool;
+            std::vector<DescriptorSetLayoutBinding *> bindings;
+        };
     } // namespace reflection
 } // namespace magma
+
+#define MAGMA_REFLECT(Type, x, ...) Type() : magma::reflection::DescriptorSetLayout(x, __VA_ARGS__) {}
