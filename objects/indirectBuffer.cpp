9adb47a4d28dd0fc49a8a9e3c2b22bdcc15e865a
v.koda <v.koda@dragonlk.com>
2021-06-21 23:10:58 +0300

    Get raw void* pointer

--- objects/indirectBuffer.cpp
+++ objects/indirectBuffer.cpp
@@ -99,9 +99,9 @@ uint32_t DrawIndirectBuffer::writeDrawCommand(const VkDrawIndirectCommand& drawC
     {
         if (mappedData)
             memcpy(mappedData + cmdCount, &drawCmd, sizeof(VkDrawIndirectCommand));
-    } else if (auto *drawIndirectCmd = memory->map<VkDrawIndirectCommand>(sizeof(VkDrawIndirectCommand) * cmdCount, sizeof(VkDrawIndirectCommand)))
+    } else if (void *data = memory->map(sizeof(VkDrawIndirectCommand) * cmdCount, sizeof(VkDrawIndirectCommand)))
     {
-        memcpy(drawIndirectCmd, &drawCmd, sizeof(VkDrawIndirectCommand));
+        memcpy(data, &drawCmd, sizeof(VkDrawIndirectCommand));
         memory->unmap();
     }
     return cmdCount++;
@@ -163,16 +163,16 @@ uint32_t DrawIndexedIndirectBuffer::writeDrawIndexedInstancedCommand(uint32_t in
     return cmdCount++;
 }
 
-uint32_t DrawIndexedIndirectBuffer::writeDrawIndexedCommand(const VkDrawIndexedIndirectCommand& drawCmd) noexcept
+uint32_t DrawIndexedIndirectBuffer::writeDrawIndexedCommand(const VkDrawIndexedIndirectCommand& drawIndexedCmd) noexcept
 {
     MAGMA_ASSERT(cmdCount < maxDrawCommands);
     if (persistentlyMapped())
     {
         if (mappedData)
-            memcpy(mappedData + cmdCount, &drawCmd, sizeof(VkDrawIndexedIndirectCommand));
-    } else if (auto *drawIndexedIndirectCmd = memory->map<VkDrawIndexedIndirectCommand>(sizeof(VkDrawIndexedIndirectCommand) * cmdCount, sizeof(VkDrawIndexedIndirectCommand)))
+            memcpy(mappedData + cmdCount, &drawIndexedCmd, sizeof(VkDrawIndexedIndirectCommand));
+    } else if (void *data = memory->map(sizeof(VkDrawIndexedIndirectCommand) * cmdCount, sizeof(VkDrawIndexedIndirectCommand)))
     {
-        memcpy(drawIndexedIndirectCmd, &drawCmd, sizeof(VkDrawIndexedIndirectCommand));
+        memcpy(data, &drawIndexedCmd, sizeof(VkDrawIndexedIndirectCommand));
         memory->unmap();
     }
     return cmdCount++;
