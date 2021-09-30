60656d7cc707dc37a274f08c2a89573b67e5599c
Victor Coda <victor.coda@gmail.com>
2021-10-01 00:19:16 +0300

    Implemented CreateInfo class - a node in the linked list for Vk*CreateInfo structure

---
+++ misc/createInfo.h
@@ -0,0 +1,35 @@
+/*
+Magma - abstraction layer to facilitate usage of Khronos Vulkan API.
+Copyright (C) 2018-2020 Victor Coda.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+#pragma once
+
+namespace magma
+{
+    /* A node in the linked list with extended creation information
+       that is assigned to pNext member of Vk*CreateInfo structure. */
+
+    class CreateInfo
+    {
+    public:
+        CreateInfo() noexcept: hash(0) {}
+        virtual const void *getNode() const noexcept { return nullptr; }
+        virtual std::size_t getHash() const noexcept { return hash; }
+
+    protected:
+        std::size_t hash;
+    };
+} // namespace magma
