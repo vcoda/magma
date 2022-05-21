afc862033f99b815aa7842ff49a67d1c025da951
Victor Coda <victor.coda@gmail.com>
2022-05-21 21:38:26 +0300

    Added sizes of depth/stencil formats

--- misc/format.cpp
+++ misc/format.cpp
@@ -258,6 +258,20 @@ std::size_t Format::nonCompressedSize() const noexcept
         return sizeof(int64_t) * 4;
     case VK_FORMAT_R64G64B64A64_SFLOAT:
         return sizeof(double) * 4;
+    // Depth/stencil formats
+    case VK_FORMAT_S8_UINT:
+        return sizeof(uint8_t);
+    case VK_FORMAT_D16_UNORM:
+        return sizeof(uint16_t);
+    case VK_FORMAT_D16_UNORM_S8_UINT:
+        return sizeof(uint16_t) + sizeof(uint8_t); // 24-bit
+    case VK_FORMAT_D24_UNORM_S8_UINT:
+    case VK_FORMAT_X8_D24_UNORM_PACK32:
+        return sizeof(uint32_t);
+    case VK_FORMAT_D32_SFLOAT:
+        return sizeof(float);
+    case VK_FORMAT_D32_SFLOAT_S8_UINT:
+        return sizeof(float) + sizeof(uint32_t); // 64-bit (24 bits are unused)
     default:
         return 0;
     }
