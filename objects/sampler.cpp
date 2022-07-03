f91c4166398dd9ad7847b05bbca4048c2829d05d
Victor Coda <victor.coda@gmail.com>
2022-07-03 03:57:56 +0300

    Do not use custom border color with unnormalized sampler because default addressing mode is VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE

--- objects/sampler.cpp
+++ objects/sampler.cpp
@@ -106,6 +106,7 @@ UnnormalizedSampler::UnnormalizedSampler(std::shared_ptr<Device> device, bool li
     const BorderColor& borderColor /* opaqueBlackFloat */):
     Sampler(std::move(device), std::move(allocator))
 {
+    MAGMA_ASSERT(!borderColor.custom());
     VkSamplerCreateInfo samplerInfo;
     samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
     samplerInfo.pNext = nullptr;
@@ -125,17 +126,6 @@ UnnormalizedSampler::UnnormalizedSampler(std::shared_ptr<Device> device, bool li
     samplerInfo.maxLod = 0.f;
     samplerInfo.borderColor = borderColor.getColor();
     samplerInfo.unnormalizedCoordinates = VK_TRUE;
-#ifdef VK_EXT_custom_border_color
-    VkSamplerCustomBorderColorCreateInfoEXT customBorderColor;
-    if (borderColor.custom())
-    {
-        customBorderColor.sType = VK_STRUCTURE_TYPE_SAMPLER_CUSTOM_BORDER_COLOR_CREATE_INFO_EXT;
-        customBorderColor.pNext = nullptr;
-        customBorderColor.customBorderColor = borderColor.getCustomColor();
-        customBorderColor.format = borderColor.getFormat();
-        samplerInfo.pNext = &customBorderColor;
-    }
-#endif // VK_EXT_custom_border_color
     const VkResult result = vkCreateSampler(MAGMA_HANDLE(device), &samplerInfo, MAGMA_OPTIONAL_INSTANCE(hostAllocator), &handle);
     MAGMA_THROW_FAILURE(result, "failed to create unnormalized sampler");
 }
