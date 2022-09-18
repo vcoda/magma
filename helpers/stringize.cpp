b244de7f7a530496d55e2a6205406b8e8134995a
Victor Coda <victor.coda@gmail.com>
2022-09-18 18:46:04 +0300

    Implemented helper functions that stringifies ORed create and usage flags

--- helpers/stringize.cpp
+++ helpers/stringize.cpp
@@ -1730,5 +1730,120 @@ const char *stringize(SpvReflectGenerator generator) noexcept
     MAGMA_DEFAULT_UNKNOWN;
     }
 }
+
+template<class FlagBits>
+inline static void stringifyOredFlagsBit(VkFlags flags, FlagBits bit, std::string& str)
+{
+    if (flags & bit)
+    {
+        if (!str.empty())
+            str += " | ";
+        str += stringize(bit);
+    }
+}
+
+std::string stringifyImageCreateFlags(VkImageCreateFlags flags)
+{
+    std::string str;
+    for (VkImageCreateFlagBits bit : {
+        VK_IMAGE_CREATE_SPARSE_BINDING_BIT,
+        VK_IMAGE_CREATE_SPARSE_RESIDENCY_BIT,
+        VK_IMAGE_CREATE_SPARSE_ALIASED_BIT,
+        VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT,
+        VK_IMAGE_CREATE_CUBE_COMPATIBLE_BIT,
+        })
+    {
+        stringifyOredFlagsBit(flags, bit, str);
+    }
+    return str;
+}
+
+std::string stringifyImageUsageFlags(VkImageUsageFlags flags)
+{
+    std::string str;
+    for (VkImageUsageFlagBits bit: {
+        VK_IMAGE_USAGE_TRANSFER_SRC_BIT,
+        VK_IMAGE_USAGE_TRANSFER_DST_BIT,
+        VK_IMAGE_USAGE_SAMPLED_BIT,
+        VK_IMAGE_USAGE_STORAGE_BIT,
+        VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+        VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
+        VK_IMAGE_USAGE_TRANSIENT_ATTACHMENT_BIT,
+        VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT,
+    #ifdef VK_EXT_fragment_density_map
+        VK_IMAGE_USAGE_FRAGMENT_DENSITY_MAP_BIT_EXT,
+    #endif
+    #ifdef VK_NV_shading_rate_image
+        VK_IMAGE_USAGE_SHADING_RATE_IMAGE_BIT_NV,
+    #endif
+    #ifdef VK_HUAWEI_invocation_mask
+        VK_IMAGE_USAGE_INVOCATION_MASK_BIT_HUAWEI,
+    #endif
+        })
+    {
+        stringifyOredFlagsBit(flags, bit, str);
+    }
+    return str;
+}
+
+std::string stringifyBufferCreateFlags(VkBufferCreateFlags flags)
+{
+    std::string str;
+    for (VkBufferCreateFlagBits bit: {
+        VK_BUFFER_CREATE_SPARSE_BINDING_BIT,
+        VK_BUFFER_CREATE_SPARSE_RESIDENCY_BIT,
+        VK_BUFFER_CREATE_SPARSE_ALIASED_BIT,
+    #ifdef VK_VERSION_1_2
+        VK_BUFFER_CREATE_PROTECTED_BIT,
+    #endif
+    #ifdef VK_EXT_buffer_device_address
+        VK_BUFFER_CREATE_DEVICE_ADDRESS_CAPTURE_REPLAY_BIT_EXT,
+    #endif
+    })
+    {
+        stringifyOredFlagsBit(flags, bit, str);
+    }
+    return str;
+}
+
+std::string stringifyBufferUsageFlags(VkBufferUsageFlags flags)
+{
+    std::string str;
+    for (VkBufferUsageFlagBits bit: {
+        VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+        VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+        VK_BUFFER_USAGE_UNIFORM_TEXEL_BUFFER_BIT,
+        VK_BUFFER_USAGE_STORAGE_TEXEL_BUFFER_BIT,
+        VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+        VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
+        VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+        VK_BUFFER_USAGE_INDIRECT_BUFFER_BIT,
+    #ifdef VK_EXT_transform_feedback
+        VK_BUFFER_USAGE_TRANSFORM_FEEDBACK_BUFFER_BIT_EXT,
+        VK_BUFFER_USAGE_TRANSFORM_FEEDBACK_COUNTER_BUFFER_BIT_EXT,
+    #endif
+    #ifdef VK_EXT_conditional_rendering
+        VK_BUFFER_USAGE_CONDITIONAL_RENDERING_BIT_EXT,
+    #endif
+    #ifdef VK_KHR_acceleration_structure
+        VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_BUILD_INPUT_READ_ONLY_BIT_KHR,
+        VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_STORAGE_BIT_KHR,
+    #endif
+    #ifdef VK_KHR_ray_tracing_pipeline
+        VK_BUFFER_USAGE_SHADER_BINDING_TABLE_BIT_KHR,
+    #endif
+    #ifdef VK_NV_ray_tracing
+        VK_BUFFER_USAGE_RAY_TRACING_BIT_NV,
+    #endif
+    #ifdef VK_EXT_buffer_device_address
+        VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT_EXT,
+    #endif
+        })
+    {
+        stringifyOredFlagsBit(flags, bit, str);
+    }
+    return str;
+}
 } // namespace helpers
 } // namespace magma
--- helpers/stringize.h
+++ helpers/stringize.h
@@ -123,5 +123,10 @@ namespace magma
         const char *stringize(SpvReflectDescriptorType) noexcept;
         const char *stringize(SpvReflectShaderStageFlagBits) noexcept;
         const char *stringize(SpvReflectGenerator) noexcept;
+
+        std::string stringifyImageCreateFlags(VkImageCreateFlags flags);
+        std::string stringifyImageUsageFlags(VkImageUsageFlags);
+        std::string stringifyBufferCreateFlags(VkBufferCreateFlags flags);
+        std::string stringifyBufferUsageFlags(VkBufferUsageFlags);
     } // namespace helpers
 } // namespace magma
