aef78138ea6fef78ee1a6061cf1e189842cfb1fd
Victor Coda <magma.github@gmail.com>
2023-08-27 19:49:41 +0300

    Added <cmath> include for std::roundf() in case if SSE not used

--- packed/vector.h
+++ packed/vector.h
@@ -16,6 +16,9 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 #pragma once
+#if !defined(MAGMA_SSE)
+#include <cmath>
+#endif
 
 namespace magma
 {
