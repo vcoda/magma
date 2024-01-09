6a7d6f0377124aaed4db5652e5f8da0af32aeba9
Victor Coda <victor.coda@gmail.com>
2024-01-09 21:37:09 +0200

    Added getter Image::getExtent()

--- src/objects/image.h
+++ src/objects/image.h
@@ -53,6 +53,7 @@ namespace magma
         uint32_t getArrayLayers() const noexcept { return arrayLayers; }
         uint32_t getSamples() const noexcept { return samples; }
         VkImageUsageFlags getUsage() const noexcept { return usage; }
+        VkExtent3D getExtent() const noexcept { return extent; }
         VkExtent3D calculateMipExtent(uint32_t level) const noexcept;
         const std::vector<VkFormat>& getViewFormats() const noexcept { return viewFormats; }
         VkSubresourceLayout getSubresourceLayout(uint32_t mipLevel,
