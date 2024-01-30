52d8f236dba41efb6bdc75ad68586283199e2137
Victor Coda <victor.coda@gmail.com>
2024-01-30 09:28:49 +0200

    Check that image was created with correct usage flag

--- src/barriers/imageMemoryBarrier.cpp
+++ src/barriers/imageMemoryBarrier.cpp
@@ -91,9 +91,11 @@ ImageMemoryBarrier::ImageMemoryBarrier(std::shared_ptr<Image> image, VkImageLayo
         dstAccessMask = VK_ACCESS_MEMORY_READ_BIT | VK_ACCESS_MEMORY_WRITE_BIT;
         break;
     case VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL:
+        MAGMA_ASSERT(image->getUsage() & VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT);
         dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
         break;
     case VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL:
+        MAGMA_ASSERT(image->getUsage() & VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT);
         dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
         break;
     case VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL:
@@ -101,17 +103,23 @@ ImageMemoryBarrier::ImageMemoryBarrier(std::shared_ptr<Image> image, VkImageLayo
         // a depth/stencil format image allowing read only access as
         // a depth/stencil attachment or in shaders as a sampled image,
         // combined image/sampler, or input attachment.
+        MAGMA_ASSERT(image->getUsage() & VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT);
         dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT;
     case VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL:
         if (image->getUsage() & VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT)
             dstAccessMask |= VK_ACCESS_INPUT_ATTACHMENT_READ_BIT;
         else
+        {
+            MAGMA_ASSERT(image->getUsage() & VK_IMAGE_USAGE_SAMPLED_BIT);
             dstAccessMask |= VK_ACCESS_SHADER_READ_BIT;
+        }
         break;
     case VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL:
+        MAGMA_ASSERT(image->getUsage() & VK_IMAGE_USAGE_TRANSFER_SRC_BIT);
         dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
         break;
     case VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL:
+        MAGMA_ASSERT(image->getUsage() & VK_IMAGE_USAGE_TRANSFER_DST_BIT);
         dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
         break;
     case VK_IMAGE_LAYOUT_PREINITIALIZED:
