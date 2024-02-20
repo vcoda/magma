8cbe4cae93e29c64eec3262ab5deb8ca9a6f8fc2
Victor Coda <victor.coda@gmail.com>
2024-02-20 20:31:04 +0200

    Removed unused members

--- src/auxiliary/textShader.h
+++ src/auxiliary/textShader.h
@@ -67,8 +67,6 @@ namespace magma
             std::shared_ptr<DescriptorSet> descriptorSet;
             std::shared_ptr<GraphicsPipeline> pipeline;
             std::unique_ptr<DescriptorSetTable> setTable;
-            uint32_t width = 0;
-            uint32_t height = 0;
             String *strings = nullptr;
             uint32_t stringCount = 0;
             Glyph *chars = nullptr;
