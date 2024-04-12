d4a99480aae6f99eb4dc5752eca04d1fde60981f
Victor Coda <victor.coda@gmail.com>
2024-04-12 12:52:01 +0300

    Clarify that we failed to create -Vulkan- instance

--- src/objects/instance.cpp
+++ src/objects/instance.cpp
@@ -113,7 +113,7 @@ Instance::Instance(const NullTerminatedStringArray& enabledLayers, const NullTer
     switch (result)
     {
     case VK_ERROR_INITIALIZATION_FAILED:
-        throw exception::InitializationFailed("failed to create instance");
+        throw exception::InitializationFailed("failed to create Vulkan instance");
     case VK_ERROR_INCOMPATIBLE_DRIVER:
         throw exception::IncompatibleDriver("could not find a compatible Vulkan ICD");
     }
