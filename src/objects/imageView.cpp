0509cfb65b71a3aeaffe1e22a09dc3922113d03c
Victor Coda <victor.coda@gmail.com>
2024-04-27 17:01:13 +0300

    Use Image::getAspectMask() method

--- src/objects/imageView.cpp
+++ src/objects/imageView.cpp
@@ -73,14 +73,7 @@ ImageView::ImageView(std::shared_ptr<Image> image_,
         imageViewInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
         imageViewInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
     }
-    if (format.depth())
-        imageViewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
-    else if (format.stencil())
-        imageViewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_STENCIL_BIT;
-    else if (format.depthStencil())
-        imageViewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT;
-    else
-        imageViewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    imageViewInfo.subresourceRange.aspectMask = image->getAspectMask();
     imageViewInfo.subresourceRange.baseMipLevel = baseMipLevel;
     imageViewInfo.subresourceRange.levelCount = levelCount;
     imageViewInfo.subresourceRange.baseArrayLayer = baseArrayLayer;
