f74724fd41e0f369b50ffd56dc52e8d6eccb6235
Victor Coda <victor.coda@gmail.com>
2024-05-17 10:37:52 +0300

    Removed redundant includes

--- src/objects/indexBuffer.cpp
+++ src/objects/indexBuffer.cpp
@@ -17,7 +17,6 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 #include "pch.h"
 #pragma hdrstop
-#include "device.h"
 #include "indexBuffer.h"
 #include "srcTransferBuffer.h"
 #include "commandBuffer.h"
