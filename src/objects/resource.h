5927fd0c006b95e4f8eb4809344423fb6fab419a
Victor Coda <victor.coda@gmail.com>
2024-09-01 12:55:29 +0300

    Include missing errorResult.h and allocator.h headers to avoid errors if precompiled header not used

--- src/objects/resource.h
+++ src/objects/resource.h
@@ -19,6 +19,7 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 #include "iresource.h"
 #include "nondispatchable.h"
 #include "../misc/sharing.h"
+#include "../allocator/allocator.h"
 
 namespace magma
 {
