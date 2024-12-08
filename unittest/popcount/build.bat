5f5cb3491e4c419a994189aeeb0d6373a5588b6e
Victor Coda <victor.coda@gmail.com>
2024-12-08 14:45:28 +0200

    Added standard GPL header to each .bat file

--- unittest/popcount/build.bat
+++ unittest/popcount/build.bat
@@ -1,2 +1,18 @@
-REM Run in x64 Native Tools Command Prompt
+@echo off
+rem Magma - Abstraction layer over Khronos Vulkan API.
+rem Copyright (C) 2018-2024 Victor Coda.
+
+rem This program is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation, either version 3 of the License, or
+rem (at your option) any later version.
+
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+rem GNU General Public License for more details.
+
+rem You should have received a copy of the GNU General Public License
+rem along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 cl /std:c++17 /I%VK_SDK_PATH%\Include /I..\..\src /EHsc unittest.cpp /link
