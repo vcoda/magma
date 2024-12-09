72afba870dc58bb1530ea4bcc88c45c66c8601fd
Victor Coda <victor.coda@gmail.com>
2024-12-10 01:22:29 +0200

    iresource.h header moved to src/objects/model directory

--- src/objects/resource.h
+++ src/objects/resource.h
@@ -16,8 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 #pragma once
-#include "iresource.h"
 #include "ideviceMemory.h"
+#include "model/iresource.h"
 #include "model/nondispatchable.h"
 #include "../misc/sharing.h"
 #include "../allocator/allocator.h"
