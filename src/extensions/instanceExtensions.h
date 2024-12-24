a7b2130847c3cffe01c9bd36fa2f8257da1004b1
Victor Coda <victor.coda@gmail.com>
2024-12-24 13:16:39 +0200

    Changed spacing

--- src/extensions/instanceExtensions.h
+++ src/extensions/instanceExtensions.h
@@ -29,62 +29,62 @@ namespace magma
     {
     public:
         // ANDROID (Google LLC)
-        const VkBool32 ANDROID_native_buffer : 1;
+        const VkBool32 ANDROID_native_buffer: 1;
 
         // Multivendor
-        const VkBool32 EXT_acquire_drm_display : 1;
-        const VkBool32 EXT_acquire_xlib_display : 1;
-        const VkBool32 EXT_debug_report : 1;
-        const VkBool32 EXT_debug_utils : 1;
-        const VkBool32 EXT_direct_mode_display : 1;
-        const VkBool32 EXT_directfb_surface : 1;
-        const VkBool32 EXT_display_surface_counter : 1;
-        const VkBool32 EXT_layer_settings : 1;
-        const VkBool32 EXT_metal_surface : 1;
-        const VkBool32 EXT_surface_maintenance1 : 1;
-        const VkBool32 EXT_swapchain_colorspace : 1;
+        const VkBool32 EXT_acquire_drm_display: 1;
+        const VkBool32 EXT_acquire_xlib_display: 1;
+        const VkBool32 EXT_debug_report: 1;
+        const VkBool32 EXT_debug_utils: 1;
+        const VkBool32 EXT_direct_mode_display: 1;
+        const VkBool32 EXT_directfb_surface: 1;
+        const VkBool32 EXT_display_surface_counter: 1;
+        const VkBool32 EXT_layer_settings: 1;
+        const VkBool32 EXT_metal_surface: 1;
+        const VkBool32 EXT_surface_maintenance1: 1;
+        const VkBool32 EXT_swapchain_colorspace: 1;
 
         // FUCHSIA (Google LLC)
-        const VkBool32 FUCHSIA_imagepipe_surface : 1;
+        const VkBool32 FUCHSIA_imagepipe_surface: 1;
 
         // GGP (Google, LLC)
-        const VkBool32 GGP_stream_descriptor_surface : 1;
+        const VkBool32 GGP_stream_descriptor_surface: 1;
 
-        const VkBool32 GOOGLE_surfaceless_query : 1;
+        const VkBool32 GOOGLE_surfaceless_query: 1;
 
         // Khronos
-        const VkBool32 KHR_android_surface : 1;
-        const VkBool32 KHR_device_group_creation : 1;
-        const VkBool32 KHR_display : 1;
-        const VkBool32 KHR_external_fence_capabilities : 1;
-        const VkBool32 KHR_external_memory_capabilities : 1;
-        const VkBool32 KHR_external_semaphore_capabilities : 1;
-        const VkBool32 KHR_get_display_properties2 : 1;
-        const VkBool32 KHR_get_physical_device_properties2 : 1;
-        const VkBool32 KHR_get_surface_capabilities2 : 1;
-        const VkBool32 KHR_portability_enumeration : 1;
-        const VkBool32 KHR_surface : 1;
-        const VkBool32 KHR_surface_protected_capabilities : 1;
-        const VkBool32 KHR_wayland_surface : 1;
-        const VkBool32 KHR_win32_surface : 1;
-        const VkBool32 KHR_xcb_surface : 1;
-        const VkBool32 KHR_xlib_surface : 1;
+        const VkBool32 KHR_android_surface: 1;
+        const VkBool32 KHR_device_group_creation: 1;
+        const VkBool32 KHR_display: 1;
+        const VkBool32 KHR_external_fence_capabilities: 1;
+        const VkBool32 KHR_external_memory_capabilities: 1;
+        const VkBool32 KHR_external_semaphore_capabilities: 1;
+        const VkBool32 KHR_get_display_properties2: 1;
+        const VkBool32 KHR_get_physical_device_properties2: 1;
+        const VkBool32 KHR_get_surface_capabilities2: 1;
+        const VkBool32 KHR_portability_enumeration: 1;
+        const VkBool32 KHR_surface: 1;
+        const VkBool32 KHR_surface_protected_capabilities: 1;
+        const VkBool32 KHR_wayland_surface: 1;
+        const VkBool32 KHR_win32_surface: 1;
+        const VkBool32 KHR_xcb_surface: 1;
+        const VkBool32 KHR_xlib_surface: 1;
 
         // LunarG, Inc.
-        const VkBool32 LUNARG_direct_driver_loading : 1;
+        const VkBool32 LUNARG_direct_driver_loading: 1;
 
         // The Brenwill Workshop Ltd.
-        const VkBool32 MVK_ios_surface : 1;
-        const VkBool32 MVK_macos_surface : 1;
+        const VkBool32 MVK_ios_surface: 1;
+        const VkBool32 MVK_macos_surface: 1;
 
         // Nintendo Co., Ltd.
-        const VkBool32 NN_vi_surface : 1;
+        const VkBool32 NN_vi_surface: 1;
 
         // NVIDIA Corporation
-        const VkBool32 NV_external_memory_capabilities : 1;
+        const VkBool32 NV_external_memory_capabilities: 1;
 
         // BlackBerry Limited
-        const VkBool32 QNX_screen_surface : 1;
+        const VkBool32 QNX_screen_surface: 1;
 
         InstanceExtensions(const char *layerName = nullptr);
     };
--- src/extensions/instanceLayers.h
+++ src/extensions/instanceLayers.h
@@ -26,123 +26,123 @@ namespace magma
     class InstanceLayers : public Layers
     {
     public:
-        const VkBool32 GalaxyOverlayVkLayer : 1;
-        const VkBool32 GalaxyOverlayVkLayer_DEBUG : 1;
-        const VkBool32 GalaxyOverlayVkLayer_VERBOSE : 1;
+        const VkBool32 GalaxyOverlayVkLayer: 1;
+        const VkBool32 GalaxyOverlayVkLayer_DEBUG: 1;
+        const VkBool32 GalaxyOverlayVkLayer_VERBOSE: 1;
 
-        const VkBool32 GraphicsSpy : 1;
-        const VkBool32 libVK_LAYER_RGA_pipeline_extraction : 1;
-        const VkBool32 MangoHud : 1;
-        const VkBool32 MoltenVK : 1;
-        const VkBool32 VirtualSwapchain : 1;
+        const VkBool32 GraphicsSpy: 1;
+        const VkBool32 libVK_LAYER_RGA_pipeline_extraction: 1;
+        const VkBool32 MangoHud: 1;
+        const VkBool32 MoltenVK: 1;
+        const VkBool32 VirtualSwapchain: 1;
 
-        const VkBool32 AEJS_DeviceChooserLayer : 1;
+        const VkBool32 AEJS_DeviceChooserLayer: 1;
 
-        const VkBool32 AMD_switchable_graphics : 1;
-        const VkBool32 AMD_switchable_graphics_32 : 1;
-        const VkBool32 AMD_switchable_graphics_64 : 1;
+        const VkBool32 AMD_switchable_graphics: 1;
+        const VkBool32 AMD_switchable_graphics_32: 1;
+        const VkBool32 AMD_switchable_graphics_64: 1;
 
-        const VkBool32 bandicam_helper : 1;
-        const VkBool32 com_xsplit_www_xbc_64_1 : 1;
-        const VkBool32 DxtoryHookHelper : 1;
-        const VkBool32 EOS_Overlay : 1;
-        const VkBool32 FCAT_DT_overlay64 : 1;
-        const VkBool32 FLIMES : 1;
-        const VkBool32 force_priority : 1;
-        const VkBool32 fossilize : 1;
-        const VkBool32 fpsmon : 1;
-        const VkBool32 FPSMonitor : 1;
+        const VkBool32 bandicam_helper: 1;
+        const VkBool32 com_xsplit_www_xbc_64_1: 1;
+        const VkBool32 DxtoryHookHelper: 1;
+        const VkBool32 EOS_Overlay: 1;
+        const VkBool32 FCAT_DT_overlay64: 1;
+        const VkBool32 FLIMES: 1;
+        const VkBool32 force_priority: 1;
+        const VkBool32 fossilize: 1;
+        const VkBool32 fpsmon: 1;
+        const VkBool32 FPSMonitor: 1;
 
-        const VkBool32 Galaxy_Overlay : 1;
-        const VkBool32 Galaxy_Overlay_DEBUG : 1;
-        const VkBool32 Galaxy_Overlay_VERBOSE : 1;
+        const VkBool32 Galaxy_Overlay: 1;
+        const VkBool32 Galaxy_Overlay_DEBUG: 1;
+        const VkBool32 Galaxy_Overlay_VERBOSE: 1;
 
-        const VkBool32 GOOGLE_threading : 1;
-        const VkBool32 GOOGLE_unique_objects : 1;
+        const VkBool32 GOOGLE_threading: 1;
+        const VkBool32 GOOGLE_unique_objects: 1;
 
-        const VkBool32 gshade : 1;
+        const VkBool32 gshade: 1;
 
         const VkBool32 INTEL_nullhw	: 1;
-        const VkBool32 INTEL_state_tracker : 1;
+        const VkBool32 INTEL_state_tracker: 1;
 
         // https://github.com/KhronosGroup/Vulkan-ExtensionLayer
-        const VkBool32 KHRONOS_memory_decompression : 1;
-        const VkBool32 KHRONOS_profiles : 1;
-        const VkBool32 KHRONOS_shader_object : 1;
-        const VkBool32 KHRONOS_synchronization2 : 1;
-        const VkBool32 KHRONOS_timeline_semaphore : 1;
-        const VkBool32 KHRONOS_validation : 1;
+        const VkBool32 KHRONOS_memory_decompression: 1;
+        const VkBool32 KHRONOS_profiles: 1;
+        const VkBool32 KHRONOS_shader_object: 1;
+        const VkBool32 KHRONOS_synchronization2: 1;
+        const VkBool32 KHRONOS_timeline_semaphore: 1;
+        const VkBool32 KHRONOS_validation: 1;
 
-        const VkBool32 LUNARG_api_dump : 1;
-        const VkBool32 LUNARG_assistant_layer : 1;
-        const VkBool32 LUNARG_core_validation : 1;
-        const VkBool32 LUNARG_demo_layer : 1;
-        const VkBool32 LUNARG_device_profile_api : 1;
-        const VkBool32 LUNARG_device_simulation : 1;
-        const VkBool32 LUNARG_gfxreconstruct : 1;
-        const VkBool32 LUNARG_image : 1;
-        const VkBool32 LUNARG_monitor : 1;
-        const VkBool32 LUNARG_object_tracker : 1;
-        const VkBool32 LUNARG_overlay : 1;
-        const VkBool32 LUNARG_override : 1;
-        const VkBool32 LUNARG_parameter_validation : 1;
-        const VkBool32 LUNARG_screenshot : 1;
-        const VkBool32 LUNARG_standard_validation : 1;
-        const VkBool32 LUNARG_starter_layer : 1;
-        const VkBool32 LUNARG_swapchain : 1;
-        const VkBool32 LUNARG_TestingIntercept : 1;
-        const VkBool32 LUNARG_VkGHL32 : 1;
-        const VkBool32 LUNARG_VkGHL64 : 1;
-        const VkBool32 LUNARG_vktrace : 1;
+        const VkBool32 LUNARG_api_dump: 1;
+        const VkBool32 LUNARG_assistant_layer: 1;
+        const VkBool32 LUNARG_core_validation: 1;
+        const VkBool32 LUNARG_demo_layer: 1;
+        const VkBool32 LUNARG_device_profile_api: 1;
+        const VkBool32 LUNARG_device_simulation: 1;
+        const VkBool32 LUNARG_gfxreconstruct: 1;
+        const VkBool32 LUNARG_image: 1;
+        const VkBool32 LUNARG_monitor: 1;
+        const VkBool32 LUNARG_object_tracker: 1;
+        const VkBool32 LUNARG_overlay: 1;
+        const VkBool32 LUNARG_override: 1;
+        const VkBool32 LUNARG_parameter_validation: 1;
+        const VkBool32 LUNARG_screenshot: 1;
+        const VkBool32 LUNARG_standard_validation: 1;
+        const VkBool32 LUNARG_starter_layer: 1;
+        const VkBool32 LUNARG_swapchain: 1;
+        const VkBool32 LUNARG_TestingIntercept: 1;
+        const VkBool32 LUNARG_VkGHL32: 1;
+        const VkBool32 LUNARG_VkGHL64: 1;
+        const VkBool32 LUNARG_vktrace: 1;
 
         const VkBool32 MANGOAPP_overlay	: 1;
-        const VkBool32 MANGOHUD32_overlay : 1;
-        const VkBool32 MANGOHUD_overlay : 1;
+        const VkBool32 MANGOHUD32_overlay: 1;
+        const VkBool32 MANGOHUD_overlay: 1;
 
-        const VkBool32 MESA_device_select : 1;
-        const VkBool32 MESA_overlay : 1;
+        const VkBool32 MESA_device_select: 1;
+        const VkBool32 MESA_overlay: 1;
 
-        const VkBool32 NV_nomad : 1;
-        const VkBool32 NV_nsight : 1;
-        const VkBool32 NV_nsight_sys : 1;
-        const VkBool32 NV_optimus : 1;
-        const VkBool32 NV_vgd : 1;
-        const VkBool32 Nvidia_Shader_Dump : 1;
+        const VkBool32 NV_nomad: 1;
+        const VkBool32 NV_nsight: 1;
+        const VkBool32 NV_nsight_sys: 1;
+        const VkBool32 NV_optimus: 1;
+        const VkBool32 NV_vgd: 1;
+        const VkBool32 Nvidia_Shader_Dump: 1;
 
-        const VkBool32 OBS_HOOK : 1;
-        const VkBool32 OBS_vkcapture_32 : 1;
-        const VkBool32 OBS_vkcapture_64 : 1;
-        const VkBool32 OW_OBS_HOOK : 1;
-        const VkBool32 OW_OVERLAY : 1;
+        const VkBool32 OBS_HOOK: 1;
+        const VkBool32 OBS_vkcapture_32: 1;
+        const VkBool32 OBS_vkcapture_64: 1;
+        const VkBool32 OW_OBS_HOOK: 1;
+        const VkBool32 OW_OVERLAY: 1;
 
-        const VkBool32 playclaw : 1;
+        const VkBool32 playclaw: 1;
 
-        const VkBool32 PRIMUS_PrimusVK : 1;
-        const VkBool32 RENDERDOC_Capture : 1;
-        const VkBool32 reshade : 1;
-        const VkBool32 reshade32 : 1;
-        const VkBool32 RGA_pipeline_extraction : 1;
-        const VkBool32 ROCKSTAR_GAMES_social_club : 1;
-        const VkBool32 RTSS : 1;
+        const VkBool32 PRIMUS_PrimusVK: 1;
+        const VkBool32 RENDERDOC_Capture: 1;
+        const VkBool32 reshade: 1;
+        const VkBool32 reshade32: 1;
+        const VkBool32 RGA_pipeline_extraction: 1;
+        const VkBool32 ROCKSTAR_GAMES_social_club: 1;
+        const VkBool32 RTSS: 1;
 
-        const VkBool32 VALVE_steam_fossilize : 1;
-        const VkBool32 VALVE_steam_fossilize_32 : 1;
-        const VkBool32 VALVE_steam_fossilize_64 : 1;
-        const VkBool32 VALVE_steam_overlay : 1;
-        const VkBool32 VALVE_steam_overlay_32 : 1;
-        const VkBool32 VALVE_steam_overlay_64 : 1;
+        const VkBool32 VALVE_steam_fossilize: 1;
+        const VkBool32 VALVE_steam_fossilize_32: 1;
+        const VkBool32 VALVE_steam_fossilize_64: 1;
+        const VkBool32 VALVE_steam_overlay: 1;
+        const VkBool32 VALVE_steam_overlay_32: 1;
+        const VkBool32 VALVE_steam_overlay_64: 1;
 
-        const VkBool32 VKBASALT_post_processing : 1;
-        const VkBool32 VKBASALT_PostProcess32 : 1;
-        const VkBool32 VKBASALT_PostProcess64 : 1;
+        const VkBool32 VKBASALT_post_processing: 1;
+        const VkBool32 VKBASALT_PostProcess32: 1;
+        const VkBool32 VKBASALT_PostProcess64: 1;
 
-        const VkBool32 vkpipeline_db : 1;
-        const VkBool32 window_system_integration : 1;
-        const VkBool32 XGCV4_64 : 1;
-        const VkBool32 XSPLIT_64 : 1;
-        const VkBool32 MIRILLIS_LAYER : 1;
+        const VkBool32 vkpipeline_db: 1;
+        const VkBool32 window_system_integration: 1;
+        const VkBool32 XGCV4_64: 1;
+        const VkBool32 XSPLIT_64: 1;
+        const VkBool32 MIRILLIS_LAYER: 1;
 
-        const VkBool32 vkdto_x86_64 : 1;
+        const VkBool32 vkdto_x86_64: 1;
 
         InstanceLayers();
     };
