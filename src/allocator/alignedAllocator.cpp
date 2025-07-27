cc97563ce6c443f46e0b2858cacdf2eb8e00ab82
Victor Coda <victor.coda@gmail.com>
2025-07-27 14:51:22 +0300

    Use __mingw_aligned_malloc/__mingw_aligned_free on MinGW plaftorm. Unix lacks aligned reallocation, so use memory copy between old and new allocated blocks. Check that alignment is power of 2.

--- src/allocator/alignedAllocator.cpp
+++ src/allocator/alignedAllocator.cpp
@@ -1,6 +1,6 @@
 /*
 Magma - Abstraction layer over Khronos Vulkan API.
-Copyright (C) 2018-2024 Victor Coda.
+Copyright (C) 2018-2025 Victor Coda.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -24,14 +24,19 @@ namespace magma
 void *AlignedAllocator::alloc(std::size_t size, std::size_t alignment,
     VkSystemAllocationScope /* allocationScope */)
 {
-#if defined(_MSC_VER) || defined(__MINGW32__)
-    void *ptr = _aligned_malloc(size, alignment);
-    if (!ptr)
+    MAGMA_ASSERT(core::powerOfTwo(alignment));
+    void *ptr;
+#if defined(_MSC_VER)
+    ptr = _aligned_malloc(size, alignment);
+#elif defined(__MINGW32__)
+    ptr = __mingw_aligned_malloc(size, alignment);
 #else
-    void *ptr = nullptr;
+    MAGMA_ASSERT(alignment % sizeof(void *) == 0);
     const int result = posix_memalign(&ptr, alignment, size);
-    if (result != 0)
-#endif // _MSC_VER
+    if (result != 0) // posix_memalign() does not modify memptr on failure
+        ptr = nullptr;
+#endif // _MSC_VER || __MINGW32__
+    if (!ptr)
     {
     #ifdef MAGMA_NO_EXCEPTIONS
         return nullptr;
@@ -43,15 +48,38 @@ void *AlignedAllocator::alloc(std::size_t size, std::size_t alignment,
 }
 
 void *AlignedAllocator::realloc(void *original, std::size_t size, std::size_t alignment,
-    VkSystemAllocationScope /* allocationScope */)
+    VkSystemAllocationScope allocationScope)
 {
-#if defined(_MSC_VER) || defined(__MINGW32__)
-    void *ptr = _aligned_realloc(original, size, alignment);
+    if (0 == size)
+    {   // realloc(ptr, 0) equals to free()
+        this->free(original);
+        return nullptr;
+    }
+    if (!original)
+        return alloc(size, alignment, allocationScope);
+    MAGMA_ASSERT(core::powerOfTwo(alignment));
+    void *ptr;
+#if defined(_MSC_VER)
+    ptr = _aligned_realloc(original, size, alignment);
 #else
-    MAGMA_UNUSED(alignment);
-    void *ptr = ::realloc(original, size);
-    // TODO: check alignment, use posix_memalign/memcpy if not aligned!
-#endif // _MSC_VER
+    #if defined(__MINGW32__)
+    ptr = __mingw_aligned_malloc(size, alignment);
+    #else
+    MAGMA_ASSERT(alignment % sizeof(void *) == 0);
+    const int result = posix_memalign(&ptr, alignment, size);
+    if (result != 0) // posix_memalign() does not modify memptr on failure
+        ptr = nullptr;
+    #endif
+    if (ptr)
+    {   // On Unix we need to copy memory between aligned blocks
+        memcpy(ptr, original, size);
+    #if defined(__MINGW32__)
+        __mingw_aligned_free(original);
+    #else
+        ::free(original);
+    #endif
+    }
+#endif // !_MSC_VER
     if (!ptr)
     {
     #ifdef MAGMA_NO_EXCEPTIONS
@@ -65,8 +93,12 @@ void *AlignedAllocator::realloc(void *original, std::size_t size, std::size_t al
 
 void AlignedAllocator::free(void *ptr) noexcept
 {
-#if defined(_MSC_VER) || defined(__MINGW32__)
+    if (!ptr)
+        return;
+#if defined(_MSC_VER)
     _aligned_free(ptr);
+#elif defined(__MINGW32__)
+    __mingw_aligned_free(ptr);
 #else
     ::free(ptr);
 #endif
