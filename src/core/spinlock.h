5c64b386737b3986057d00a1e723a12bf2e5ae6c
Victor Coda <victor.coda@gmail.com>
2025-08-02 22:14:56 +0300

    Keep headers clean, implementation moved to .inl file

--- src/core/spinlock.h
+++ src/core/spinlock.h
@@ -15,7 +15,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
-#include <atomic>
 #pragma once
 #if defined(_M_IX86) || defined(_M_X64) || defined(__i386__) || defined(__x86_64__)
     #include <emmintrin.h>
@@ -34,51 +33,12 @@ namespace magma::core
     class Spinlock
     {
     public:
-        void lock() noexcept
-        {
-            while (flag.test_and_set(std::memory_order_acquire))
-            { /* The pause intrinsic is used in spin-wait loops
-                 with the processors implementing dynamic execution
-                 (especially out-of-order execution). In the spin-
-                 wait loop, the pause intrinsic improves the speed
-                 at which the code detects the release of the lock
-                 and provides especially significant performance gain.
-                 The execution of the next instruction is delayed
-                 for an implementation-specific amount of time. */
-            #if defined(_M_IX86) || defined(_M_X64) || defined(__i386__) || defined(__x86_64__)
-                _mm_pause();
-            #elif defined(_M_ARM) || defined(_M_ARM64)
-             /* In a Symmetric Multi-Threading (SMT) design,
-                a thread can use a Yield instruction to give a hint
-                to the processor that it is running on. The Yield
-                hint indicates that whatever the thread is currently
-                doing is of low importance, and so could yield. For
-                example, the thread might be sitting in a spin-lock. */
-                __yield();
-            #elif defined(__arm__) || defined(__aarch64__)
-                #if defined(__ARM_ACLE)
-                    __yield();
-                #elif defined(__has_builtin)
-                    #if __has_builtin(__builtin_arm_yield)
-                        __builtin_arm_yield();
-                    #else
-                        __asm__ __volatile__("yield");
-                    #endif
-                #else
-                    __asm__ __volatile__("yield");
-                #endif
-            #else // !x86-64 && !ARM
-                std::this_thread::yield();
-            #endif
-            }
-        }
-
-        void unlock() noexcept
-        {
-            flag.clear(std::memory_order_release);
-        }
+        void lock() noexcept;
+        void unlock() noexcept;
 
     private:
         std::atomic_flag flag = ATOMIC_FLAG_INIT;
     };
 } // namespace magma::core
+
+#include "spinlock.inl"
